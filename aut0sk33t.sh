86ed1b9016e4f60410f8ff378cdb0d85e998c112
Lennie Budgell <868085+Bedrovelsen@users.noreply.github.com>
2023-02-16 11:56:52 -0600

    Update aut0sk33t.sh

--- aut0sk33t.sh
+++ aut0sk33t.sh
@@ -1,25 +1,34 @@
-#/bin/bash
+#!/bin/bash
 
 TARGET=$1
+echo -e "A'ight homie! We aut0k33tin $TARGET\n\n" 
 
-echo -e "G'nna start gripping subdomaynes yo! \n"
+echo -e "G'nna start gripping subdomaynes yo!\n\n"
 
-assetfinder --subs-only "$TARGET" | tee tmpsubz.txt &6 crobat -s "$TARGET" | tee -a tmpsubz.txt
+echo -e "We gonna crobat this fools assetfinder" 
 
-sort -u < tmpsubz.txt | tee subz.txt &6 rm tmpsubz.txt
+assetfinder --subs-only "$TARGET" | tee tmpsubz.txt && crobat -s "$TARGET" | tee -a tmpsubz.txt && subfinder -silent -d "$TARGET" | tee -a tmpsubz.txt
 
-echo -e "Drive by dem subz to see who awake \n" echo -e "uncover anyone pretending to be sleeping \n" echo "$TARGET" | uncover -e shodan, censys -silent -o uncvr. json -json
+sort -u < tmpsubz.txt | tee subz.txt && rm tmpsubz.txt
 
-cat subz.txt uncvr.json | grepaddr -fqdn | httpx --silent -ip -fr -threads 1000 -o alive.json -json
+echo -e "Drive by dem subz to see who awake\n"
 
-cat sub.txt uncvr.json alive.json I grepaddr -ipv4 I sort -u I tee ipz.txt
+echo -e "uncover anyone pretending to be sleeping \n"
 
-nrich -o json ipz.txt | tee nriched. json
+echo "$TARGET" | uncover -e shodan,censys -silent -o uncvr.json -json
 
-echo -e "Go nuclear on anyone awake \n"
+cat subz.txt uncvr.json | grepaddr -fqdn | httpx --silent -ip -td -fhr -title -threads 1000 -o alive.json -json
+
+cat subz.txt uncvr.json alive.json | grepaddr -ipv4 | sort -u | tee ipz.txt
+
+echo -e "Guess it'z time to nrich these IPz."
+
+nrich -o json ipz.txt | tee nriched.json
+
+echo -e "Go nuclei-r on anyone awake \n"
 
 nuclei -update-templates
 
-cat uncvr.json alive.json subz.txt nriched.json I grepaddr -fqdn | sort -u | httpx --silent | tee stayalive.txt
+cat alive.json | grepaddr -fqdn | httpx --silent | tee urls.txt
 
-nuclei --silent -I stayalive.txt -s low, medium, high, critical -me resultz
+nuclei --silent -l urls.txt -s low,medium,high,critical -me resultz | tee vulns.txt
